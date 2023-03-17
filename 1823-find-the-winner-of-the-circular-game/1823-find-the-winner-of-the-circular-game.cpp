7c63da948ef65ccd4adbcc34d7a07671e56919da
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-17 22:25:11 +0200

    Time: 69 ms (24.59%), Space: 24.4 MB (13.67%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
-    struct Node{
-        int val;
-        Node* next;
-        Node(int val, Node* next = nullptr){
-            this->val = val;
-            this->next = next;
-        }  
-    };
     int findTheWinner(int n, int k) {
-        Node* root = new Node(1);
-        Node* head = root;
-        
-        for (int i = 2; i <= n; i++){
-            root->next = new Node(i);
-            root = root->next;
-            if(i == n){
-                root->next = head;
+        queue<int> q;
+        for (int i = 1; i <= n; i++)
+            q.push(i);
+        while (q.size() > 1){
+            int rem = k-1;
+            while (rem--){
+                q.push(q.front());
+                q.pop();
             }
+            q.pop();
         }
-        
-        while (n-- > 1){
-            root = head;
-            Node* prv = root;
-            for (int i = 0; i < k-1; i++){
-                prv = root;
-                root = root->next;
-            }
-            prv->next = root->next;
-            head = root->next;
-        }
-        return head->val;
+        return q.front();
     }
 };
