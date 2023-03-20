58056bd4e1429e24f2cf3b9e2219d27ce6439115
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-20 20:20:22 +0200

    Time: 244 ms (87.87%), Space: 11.4 MB (94.51%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -5,15 +5,17 @@ public:
         for (int i = 0; i < s.size(); i++) 
             suffix[s[i] - 'a']++;
         int ans = 0;
-        unordered_set<string> seen;
+        vector<vector<bool>> seen(26);
+        for (int i = 0; i < 26; i++) {
+            seen[i] = vector<bool>(26, 0);
+        }
         for (int i = 0; i < s.size(); i++){
             suffix[s[i] - 'a']--;
             for (int j = 0; j < 26; j++){
                 if (prefix[j] && suffix[j]){
-                    string pal = string(1, char('a' + j)) + s[i];
-                    if (seen.find(pal) == seen.end()){
+                    if (seen[j][s[i] - 'a'] == 0){
                         ans++;
-                        seen.insert(pal);
+                        seen[j][s[i] - 'a'] = 1;
                     }
                 }
             }
