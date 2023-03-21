37b1332da12ad1167460e6b13d8e451e871e7014
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-21 14:11:15 +0200

    Time: 242 ms (89.15%), Space: 58.9 MB (82.06%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
+        if (k > s.size()) return 0;
         unordered_set<int> masks;
-        for (int i = 0; i + k <= s.size(); i++){
-            int msk = 0;
-            for (int j = i; j < i + k; j++){
-                if (s[j] == '1') 
-                    msk |= (1<<(j-i));
-            }
-            masks.insert(msk);
-            if (masks.size() == (1<<k)) break;
+        int hashValue = 0;
+        for (int i = 0; i < k-1; i++){
+            hashValue = (hashValue << 1) | (s[i] == '1');
         }
-        return masks.size() == (1<<k);
+        for (int i = 0, j = k-1; j < s.size(); i++, j++){
+            hashValue = (hashValue << 1) | (s[j] == '1');
+            hashValue &= ((1<<k) - 1);
+            masks.insert(hashValue);
+            if (masks.size() == (1<<k)) return 1;
+        }
+        return 0;
     }
 };
