845d588ffc1da3a2872341e4f40c626ccef76e52
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-21 15:18:47 +0200

    Time: 135 ms (77.48%), Space: 49.9 MB (41.08%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -7,22 +7,14 @@ public:
         pow2[0] = 1;
         for (int i = 1; i <= n; i++)
             pow2[i] = (pow2[i-1] << 1) % mod;
-        for (int i = 0; i < n; i++){
-            int l = i, r = n-1, m, upper = -1;
-            while (l <= r){
-                m = (l+r) >> 1;
-                if (nums[m] + nums[i] <= target)
-                    upper = m, l = m + 1;
-                else 
-                    r = m - 1;
-            }
-            if (upper == -1) continue;
-            int len = upper - i;
-            ans = (ans + pow2[len] % mod) % mod;
+        n--;
+        for (int i = 0; i <= n; i++){
+            while (i <= n && nums[i] + nums[n] > target)
+                n--;
+            int len = n - i;
+            if (len >= 0)
+                ans = (ans + pow2[len] % mod) % mod;
         }
-        
         return ans;
-            
-            
     }
 };
