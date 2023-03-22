b93f99c4acbc1d959f075290571cdd88fa39cefc
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-22 21:38:38 +0200

    Time: 19 ms (22.59%), Space: 12.5 MB (80.61%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string, int> frq;
-        for (int i = 0; i < words.size(); i++)
-            frq[words[i]]++;
-        sort(words.begin(), words.end(),[&](string& a, string& b){
-            if (frq[a] != frq[b])
+    unordered_map<string, int> frq;
+    struct cmp{
+      unordered_map<string, int>& frq;
+      bool operator()(const string& a, const string& b){
+          if (frq[a] != frq[b])
                 return frq[a] > frq[b];
             return a < b;
-        });
-        vector<string> topK;
-        for (int i = 0; k && i < words.size(); ){
-            topK.push_back(words[i]);
-            i++;
-            k--;
-            while (i < words.size() && words[i] == words[i-1])
-                i++;
+      }  
+    };
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        frq = unordered_map<string, int>();
+        for (int i = 0; i < words.size(); i++)
+            frq[words[i]]++;
+        cmp comp{frq};
+        priority_queue<string, vector<string>, cmp> pq(comp);
+        for (auto i: frq){
+            pq.push(i.first);
+            if (pq.size() > k)
+                pq.pop();
         }
-        return topK;
+        for (int i = k-1; i >= 0; i--)
+        {
+            words[i] = pq.top();
+            pq.pop();
+        }
+        words.erase(words.begin() + k, words.end());
+        return words;
         
     }
 };
