f5ea1e44be077a2db00316c66a0573143ea7a03e
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-22 17:54:59 +0200

    Time: 33 ms (54.92%), Space: 12.6 MB (93.75%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        priority_queue<vector<int>> pq;
-        pq.push({0, 0, src});
-        vector<vector<int>> dist(n), adj[n];
-        for (int i = 0; i < flights.size(); i++){
-            adj[flights[i][0]].push_back({flights[i][1], flights[i][2]});
-        }
-        int inf = 1e9;
-        for (int i = 0; i < n; i++)
-            dist[i] = vector<int>(k+2, inf);
-        for (int i = 0; i <= k+1; i++)
-            dist[src][i] = 0;
-        while (!pq.empty())
-        {
-            auto cur = pq.top();
-            cur[0] *= -1;
-            cur[1] *= -1;
-            pq.pop();
-            if (cur[1] > k) continue;
-            for (auto v: adj[cur[2]]){
-                if (dist[cur[2]][cur[1]] + v[1] < dist[v[0]][1+cur[1]]){
-                    dist[v[0]][1+cur[1]] = dist[cur[2]][cur[1]] + v[1];
-                    pq.push({-dist[v[0]][1+cur[1]], -1-cur[1], v[0]});
+        vector<int> dist(n, -1);
+        queue<int> q;
+        q.push(src);
+        dist[src] = 0;
+        k++;
+        while (q.size() && k){
+            int size = q.size();
+            vector<int> oldDistance = dist;
+            while (size--){
+                int cur = q.front();
+                q.pop();
+                for (int i = 0; i < flights.size(); i++){
+                    if (flights[i][0] != cur) continue;
+                    int v = flights[i][1];
+                    int c = flights[i][2];
+                    if (dist[v] == -1 || dist[v] > oldDistance[cur] + c){
+                        dist[v] = oldDistance[cur] + c;
+                        q.push(v);
+                    }
                 }
             }
+            k--;
         }
-        int mn = inf;
-        for (int i = 0; i <= k+1; i++)
-            mn = min(mn, dist[dst][i]);
-        if (mn == inf) mn = -1;
-        return mn;
+        return dist[dst];
     }
 };
