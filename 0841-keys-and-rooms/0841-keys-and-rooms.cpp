1f44bb7812019c3318b6fe79f939cedbecf67592
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-22 14:58:34 +0200

    Time: 11 ms (62.43%), Space: 10.4 MB (63.66%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool vis[1001];
-    void dfs(int node, vector<vector<int>>& rooms){
-        vis[node] = 1;
-        for (int i = 0; i < rooms[node].size(); i++){
-            if (vis[rooms[node][i]] == 0){
-                dfs(rooms[node][i], rooms);
-            }
-        }
-    }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         memset(vis, 0, sizeof vis);
-        dfs(0, rooms);
-        for (int i = 0; i < rooms.size(); i++){
-            if (!vis[i])
-                return 0;
+        queue<int> q;
+        q.push(0);
+        int visited = 1;
+        vis[0] = 1;
+        while (!q.empty()){
+            int cur = q.front();
+            q.pop();
+            for (int i = 0; i < rooms[cur].size(); i++){
+                if (!vis[rooms[cur][i]]){
+                    vis[rooms[cur][i]] = 1;
+                    q.push(rooms[cur][i]);
+                    visited++;
+                }
+            }
         }
-        return 1;
+        return visited == rooms.size();
     }
 };
