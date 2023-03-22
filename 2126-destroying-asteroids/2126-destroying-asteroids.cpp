eaeae2118775325b1ba654dfed91229de2ec6f06
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-22 16:23:15 +0200

    Time: 205 ms (98.61%), Space: 133.4 MB (5.16%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,11 +1,15 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        sort(asteroids.begin(), asteroids.end());
+        int N = 100001;
+        vector<int> frq(N, 0);
         long long curMass = mass;
-        for (int i = 0; i < asteroids.size(); i++){
-            if (curMass < asteroids[i]) return 0;
-            curMass += asteroids[i];
+        for (int i = 0; i < asteroids.size(); i++)
+            frq[asteroids[i]]++;
+        for (int i = 1; i < N && curMass < N-1; i++){
+            if (frq[i] == 0) continue;
+            if (i > curMass) return 0;
+            curMass += 1LL * frq[i] * i;
         }
         return 1;
     }
