76d6051af06f03f33baad0ca450d196ece3559ae
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-22 17:06:39 +0200

    Time: 56 ms (47.96%), Space: 12.5 MB (32.99%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int ways = 0;
-    void countWays(vector<int>&nums, int index, int curSum, int target){
+    int ways[20][40000];
+    int countWays(vector<int>&nums, int index, int curSum, int target){
         if (index == nums.size()){
-            if (curSum == target){
-                ways++;
-            }
-            return;
+            return (curSum == target);
         }
-        countWays(nums, index + 1, curSum + nums[index], target);
+        int& ret = ways[index][curSum];
+        if (~ret)
+            return ret;
+        ret = countWays(nums, index + 1, curSum + nums[index], target) + 
         countWays(nums, index + 1, curSum - nums[index], target);
+        return ret;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        ways = 0;
-        countWays(nums, 0, 0, target);
-        return ways;
+        memset(ways, -1, sizeof ways);
+        return countWays(nums, 0, 20000, target + 20000);
     }
 };
