4bbdbf5aabdc34938ae7d2c6b188e74bd68fc81e
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-25 22:28:05 +0200

    Time: 10 ms (83.52%), Space: 8.8 MB (36.10%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        
-        vector<vector<int>> indeces(10);
-        
         stack<char> stk;
         stk.push(num[0]);
         for (int i = 1; i < num.size(); i++){
@@ -13,6 +10,10 @@ public:
             }
             stk.push(num[i]);
         }
+        while (stk.size() && k){
+            k--;
+            stk.pop();
+        }
         string ret = "";
         while (!stk.empty()){
             ret += stk.top();
@@ -21,21 +22,6 @@ public:
         while (ret.size() > 0 && ret.back() == '0') 
             ret.pop_back();
         reverse(ret.begin(), ret.end());
-        num = ret;
-        for (int i = 0; i < num.size(); i++) indeces[num[i] - '0'].push_back(i);
-        for (int d = 9; d >= 0 && k; d--){
-            for (int j: indeces[d]){
-                if (k == 0) break;
-                k--;
-                num[j] = '#';
-            }
-        }
-        ret = "";
-        for (int i = 0; i < num.size(); i++) 
-        {
-            if (num[i] != '#')
-                ret += num[i];
-        }
         if (ret.size() == 0) ret += "0";
         return ret;
     }
