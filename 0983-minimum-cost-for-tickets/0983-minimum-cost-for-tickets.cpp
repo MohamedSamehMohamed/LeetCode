e4f81cd440bc9948faa32e285a2d2efacf5261a0
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-29 00:21:54 +0200

    Time: 2 ms (83.38%), Space: 9.5 MB (91.61%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int minCost[366];
-    int minSpend(vector<int>& days, vector<int>& costs, int day){
+    int minSpend(vector<int>& days, int index, vector<int>& costs, int day){
         if (day > days.back()) return 0;
         int& ret = minCost[day];
         if (~ret) return ret;
-        int index = 0;
         while (index < days.size() && days[index] < day)
             index++;
         day = days[index];
         ret = 1e9;
         int period[] = {1, 7, 30};
         for (int i = 0; i < 3; i++){
-            ret = min(ret, costs[i] + minSpend(days, costs, day + period[i]));
+            ret = min(ret, costs[i] + minSpend(days, index + 1, costs, day + period[i]));
         }
         return ret;
     }
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         memset(minCost, -1, sizeof minCost);
-        return minSpend(days, costs, 1);
+        return minSpend(days, 0, costs, 1);
     }
 };
