826a3d3253dc25ebd030bd535a5b5b9f5e15f76f
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-29 07:09:14 +0200

    Time: 13 ms (73.83%), Space: 8.1 MB (77.38%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -10,16 +10,16 @@ public:
         int p = 1;
         for (int i = 1; i < n; i++){
             maxLikeTime[p][0] = max(satisfaction[i], maxLikeTime[p^1][0]);
-            mx = max(maxLikeTime[p][0], mx);
             for (int k = 1; k <= i; k++){
                 maxLikeTime[p][k] = maxLikeTime[p^1][k];
                 maxLikeTime[p][k] = max(maxLikeTime[p][k], 
                                        maxLikeTime[p^1][k-1] + satisfaction[i] * (k+1));
-                mx = max(maxLikeTime[p][k], mx);
             }
             maxLikeTime[p][i+1] = -1e9;
             p ^= 1;
         }
+        for (int i = 0; i < n; i++)
+            mx = max(mx, maxLikeTime[p^1][i]);
         return mx;
         
     }
