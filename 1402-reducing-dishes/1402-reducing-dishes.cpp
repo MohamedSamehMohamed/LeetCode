1314e891de932a53b25f7ae52410bd432c9bcef5
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-29 07:01:55 +0200

    Time: 18 ms (69.92%), Space: 8 MB (98.26%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int maxLikeTime[2][500];
+    int maxLikeTime[2][501];
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(), satisfaction.end());
         int n = satisfaction.size();
         maxLikeTime[0][0] = satisfaction[0];
-        for (int i = 1; i < n; i++)
-            maxLikeTime[0][i] = -1e9;
+        maxLikeTime[0][1] = -1e9;
         int mx = 0;
         int p = 1;
         for (int i = 1; i < n; i++){
             maxLikeTime[p][0] = satisfaction[i];
             mx = max(maxLikeTime[p][0], mx);
-            for (int k = 1; k < n; k++){
+            for (int k = 1; k <= i; k++){
                 maxLikeTime[p][k] = maxLikeTime[p^1][k];
                 maxLikeTime[p][k] = max(maxLikeTime[p][k], 
                                        maxLikeTime[p^1][k-1] + satisfaction[i] * (k+1));
                 mx = max(maxLikeTime[p][k], mx);
             }
+            maxLikeTime[p][i+1] = -1e9;
             p ^= 1;
         }
         return mx;
