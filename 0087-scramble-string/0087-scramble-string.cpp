9d178c38281a6aa212e47929c2ac21379226c5f1
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-03-30 04:08:16 +0200

    Time: 7 ms (95.99%), Space: 6.2 MB (96.07%) - LeetHub

--- 0087-scramble-string/0087-scramble-string.cpp
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int dp[30][30][30];
-    int solve(string& a, string& b, int l, int r, int l1, int r1){
-        bool same = 1;
-        for (int i = l; i <= r; i++){
-            if (a[i] != b[l1+(i-l)]){
-                same = 0;
-                break;
-            }
-        }
-        if (same || l == r) return same;
-        int& ret = dp[l][r][l1];
+    int dp[30][30][31];
+    int solve(string& a, string& b, int l1, int l2, int len){
+        int& ret = dp[l1][l2][len];
         if (~ret) return ret;
         ret = 0;
-        for (int i = 1; ret == 0 && i < (r-l+1); i++){
-            ret |= (solve(a, b, l, l + i - 1, l1, l1 + i - 1) && solve(a, b, l + i, r, l1 + i, r1));
-            ret |= (ret || (solve(a, b, l+i, r, l1, l1 + (r-(l+i)+1) - 1) && solve(a, b, l, l+i-1, l1 + (r-(l+i)+1), r1)));
+        bool sameString = 1;
+        for (int i = 0; i < len && sameString; i++){
+            sameString &= (a[i+l1] == b[i+l2]);
+        }
+        ret = sameString;
+        if (len == 1 || sameString) return sameString;
+        for (int cut = 1; !ret && cut < len; cut++){
+            ret = (solve(a, b, l1, l2, cut) && solve(a, b, l1 + cut, l2 + cut, len - cut));
+            ret = (ret || (solve(a, b, l1+cut, l2, len - cut) && solve(a, b, l1, l2 + len - cut, cut)));
         }
         return ret;
+        
     }
     bool isScramble(string s1, string s2) {
         memset(dp, -1 ,sizeof dp);
-        return solve(s1, s2, 0, s1.size()-1, 0, s2.size()-1);
+        return solve(s1, s2, 0, 0, s1.size());
     }
 };
