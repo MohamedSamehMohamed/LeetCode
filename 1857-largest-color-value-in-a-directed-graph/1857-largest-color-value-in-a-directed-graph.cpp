3ef2a9734a17c4d62b1e040ed21a1fc40f4c2b4a
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-04-11 02:23:48 +0200

    Time: 1369 ms (5.01%), Space: 278.2 MB (5.01%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int vis[100002];
     vector<int> adj[100002];
-    int dp[100002][26];
+    map<int, vector<int>> mp;
     bool cycle;
     string _colors;
     void dfs(int u){
@@ -14,22 +14,31 @@ public:
             }
             if (!vis[v])
                 dfs(v);
-            for (int i = 0; i < 26; i++)
-                dp[u][i] = max(dp[u][i], dp[v][i]);
         }
         vis[u] = 2;
-        dp[u][_colors[u] - 'a']++;
     }
-    
+    vector<int> dfs2(int u){
+        if (mp.find(u) != mp.end()) 
+            return mp[u];
+        vector<int> values(26, 0);
+        
+        
+        for (int v: adj[u]){
+            auto child = dfs2(v);
+            for (int i = 0; i < 26; i++)
+                values[i] = max(values[i], child[i]);
+        }
+        values[_colors[u] - 'a']++;
+        return mp[u] = values;
+    }
     int largestPathValue(string colors, vector<vector<int>>& edges) {
         for (int i = 0; i < colors.size(); i++)
         {
             adj[i].clear();
             vis[i] = 0;
-            for (int j = 0; j < 26; j++)
-                dp[i][j] = 0;
         }
         cycle = 0;
+        mp.clear();
         _colors = colors;
         for (int i = 0; i < edges.size(); i++){
             int u = edges[i][0], v = edges[i][1];
@@ -43,12 +52,14 @@ public:
         if (cycle) return -1;
         int mx = 1;
         for (int i = 0; i < colors.size(); i++) 
-        {
-            for (int j = 0; j < 26; j++)
-                mx = max(mx, dp[i][j]);
+            dfs2(i);
+        
+        for (int i = 0; i < colors.size(); i++){
+           for (int j = 0; j < 26; j++)
+            {
+               mx = max(mx, mp[i][j]);
+            }
         }
-        
-        
         return mx;
         
     }
