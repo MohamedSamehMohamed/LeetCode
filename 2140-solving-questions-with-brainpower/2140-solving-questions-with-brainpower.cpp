08e8572bbd628182ea0a56549bd1899ce2059326
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-05-12 16:07:06 +0300

    Time: 385 ms (45.17%), Space: 112.6 MB (99.78%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     long long dp[100009];
-    long long solve(vector<vector<int>>& q, int i){
-        if (i >= q.size()) return 0;
-        long long& ret = dp[i];
-        if (~ret) return ret;
-        ret = max(solve(q, i + 1), 
-                 q[i][0] + solve(q, i + q[i][1] + 1));
-        return ret;
-    }
+    
     long long mostPoints(vector<vector<int>>& questions) {
-        memset(dp, -1, sizeof dp);
-        return solve(questions, 0);
+        int n = questions.size();
+        dp[n-1] = questions[n-1][0];
+        for (int i = n - 2; i >= 0; i--){
+            int pos = i + questions[i][1] + 1;
+            long long profit = (pos < n? dp[pos]: 0);
+            dp[i] = max(dp[i+1], questions[i][0] + profit);
+        }
+        return dp[0];
     }
 };
