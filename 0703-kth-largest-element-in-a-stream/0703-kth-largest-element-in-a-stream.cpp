26c547aa4ab1ddf8a52b6b1cc9c4f4aecbd8db34
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-05-23 12:57:16 +0300

    Time: 41 ms (32.81%), Space: 19.8 MB (60.44%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,29 +1,18 @@
 class KthLargest {
 public:
-    priority_queue<int> one, two;
+    priority_queue<int> pq;
     int k;
     KthLargest(int k, vector<int>& nums) {
         this->k = k;
         for (int i = 0; i < nums.size(); i++)
-            one.push(nums[i]);
+            pq.push(-nums[i]);
     }
     
     int add(int val) {
-        // put largest [k] elements in [two] pq with smallest element in top, 
-        // so that the top of [two] pq will be the largest k element  
-        // two.top() >= one.top()
-        one.push(val);
-        while (!two.empty() && -two.top() < one.top()){
-            int twoElement = -two.top(); two.pop();
-            int oneElement = one.top(); one.pop();
-            one.push(twoElement);
-            two.push(-oneElement);
-        }
-        while (two.size() < k){
-            two.push(-one.top());
-            one.pop();
-        }
-        return -two.top();
+        pq.push(-val);
+        while (pq.size() > k) 
+            pq.pop();
+        return -pq.top();
         
     }
 };
