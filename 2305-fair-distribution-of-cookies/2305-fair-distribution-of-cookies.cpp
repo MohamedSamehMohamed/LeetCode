6323026e0b94b3fde371f2ccd263033b0f9ca7d1
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-07-01 11:26:48 +0300

    Time: 1330 ms (60.48%), Space: 6.9 MB (23.17%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
-    int mn;
-    void solve(vector<int>& cookies, vector<int>& a, int index){
+    int mn, _k;
+    int a[8];
+    void solve(vector<int>& cookies, int index){
         if (index == cookies.size()){
             int mx = 0;
-            for (int i = 0; i < a.size(); i++) mx = max(mx, a[i]);
+            for (int i = 0; i < _k; i++) 
+                mx = max(mx, a[i]);
             mn = min(mn, mx);
             return;
         }
-        for (int i = 0; i < a.size(); i++){
+        for (int i = 0; i < _k; i++){
             a[i] += cookies[index];
-            solve(cookies, a, index + 1);
+            solve(cookies, index + 1);
             a[i] -= cookies[index];
         }
     }
     int distributeCookies(vector<int>& cookies, int k) {
         mn = 1e9;
-        vector<int> a(k, 0);
-        solve(cookies, a, 0);
+        _k = k;
+        memset(a, 0, sizeof a);
+        solve(cookies, 0);
         return mn;
     }
 };
