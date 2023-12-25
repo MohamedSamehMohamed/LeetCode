f3eb63575320e8906318da1a21f51bd9ac21997a
MohamedSameh <32108759+MohamedSamehMohamed@users.noreply.github.com>
2023-12-25 08:21:37 -0800

    Update 0091-decode-ways.cpp

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,5 +1,32 @@
 class Solution {
 public:
+    int numDecodings(string s) {
+        vector<int> ways(s.size());
+        ways[0] = (s[0] != '0');
+        for (int i = 1; i < s.size(); i++){
+            // can be added to the previous digit 
+            // dd -> 1x -> x {0:9}
+            // dd -> 2x -> x {0:6}
+            bool canBeAdded = 0;
+            if (s[i-1] == '1'){
+                canBeAdded = (s[i] >= '0' && s[i] <= '9');
+            }else if (s[i-1] == '2'){
+                canBeAdded = (s[i] >= '0' && s[i] <= '6');
+            }
+            // can be single 
+            bool canBeSingle = 0;
+            if (s[i] != '0')
+                canBeSingle = true;
+            
+            ways[i] = ways[i-1] * canBeSingle + 
+                    (i-2>=0? ways[i-2]: s[i-1]!='0'? 1: 0) * canBeAdded;
+        }
+        return ways[s.size()-1];
+    }
+};
+
+class Solution_Recursive {
+public:
     int dp[101];
     int solve(string& s, int i = 0)
     {
